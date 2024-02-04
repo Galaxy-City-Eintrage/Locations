6dcee47c2cd255a8b615ab960a2e10dbf2500ed0
Domi <106663559+easydomii@users.noreply.github.com>
2024-02-04 17:35:47 +0100

    Create TuiningPunkte.lua

---
+++ Tuining/TuiningPunkte.lua
@@ -0,0 +1,26 @@
+-- Für Job Tuiningpunkte
+
+    {
+            name        = "Mechaniker 1",
+            jobs        = { "mechanic" },
+            coords      = vector3(-323.7714, -965.4063, 31.1637),
+            size        = vector3(5, 5, 5),
+            rotation    = 70,
+            type        = "job",
+            debug       = false,
+            blipIcon    = 72,
+            blipColor   = 3,
+            blipDisplay = 4,
+            blipScale   = 0.7, 
+            blipEnabled = false,
+            premium     = 0, -- 0 premium price for job zones
+            isFree      = false,
+            hideMods = {
+                  --["modEngine"] = true,
+                  --["modLivery"] = true
+            }
+      },
+
+
+
+-- Für Selfservice
