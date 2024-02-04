343e8d94489401c0e79c68cb93423ca1f25252e6
Domi <106663559+easydomii@users.noreply.github.com>
2024-02-04 17:40:44 +0100

    Create Bossmenu.lua

---
+++ Unternehme/Bossmenu.lua
@@ -0,0 +1,50 @@
+target = { -- optional you can leave it empty if you are not using qtarget
+        police_target = {
+            job = 'police',
+            coords = vector3(-401.72, -343.8, 32.4),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 160.0
+        },
+        mechanic_target = {
+            job = 'mechanic',
+            coords = vector3(-307.2, -931.88, 32.08),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 0.0
+        },
+        fib_target = {
+            job = 'fib',
+            coords = vector3(-389.3538513183594,-337.1472473144531,52.2402343759),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 255.11
+        },
+        cali_target = {
+            job = 'cali',
+            coords = vector3(-1528.64, 151.24, 59.8),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 255.11
+        },
+        goodfellas_target = {
+            job = 'goodfellas',
+            coords = vector3(796.8, -750.2, 31.28),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 255.11
+        },
+        mg13_target = {
+            job = 'mg13',
+            coords = vector3(308.76, -2029.96, 20.64),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 304.16
+        },
+        ambulance_target = {
+            job = 'ambulance',
+            coords = vector3(-508.88, -300.32, 69.44),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 255.11
+        },
+        goldenpub_target = {
+            job = 'goldenpub',
+            coords = vector3(1204.24, -501.8, 65.32),
+            size = vector3(1.5, 1.5, 2.0),
+            heading = 255.11
+        },
+    },
