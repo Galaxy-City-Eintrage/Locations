bd0fe4caf21cffcf4c35de94ea0bc58d3d3c283f
Domi <106663559+easydomii@users.noreply.github.com>
2024-02-04 17:32:02 +0100

    Create JobGarage.lua

---
+++ Garagen/JobGarage.lua
@@ -0,0 +1,31 @@
+{
+        job = 'mechanic',
+        name = 'mechanic-car',
+        grade = 6,
+        price = 0, -- Put a price or 0
+        coords = {
+            menuCoords = vec3(-283.4, -919.16, 31.12),
+            spawnCoords = vec4(-289.04, -909.6, 31.12, 337.2),
+        },
+        vehicles = {
+            --`slamtruck`,
+            --`flatbed`,
+        },
+        liveries = {
+            --[`slamtruck`] = 1
+        },
+        tuning = {},
+        extras = {},
+        vehicleCamera = {
+            vehicleCoords = vec4(-197.353851, -1304.004395, 30.981689, 291.968506),
+            camera = {
+                coords = vec3(-194.782410, -1299.046143, 31.285034),
+                rotation = vec3(3.0, 2.0, 170.0),
+                ped = vec4(-198.184616, -1302.672485, 31.285034, 325.55)
+            }
+        },
+        cinematicCams = {
+            vec3(280.496704, -607.186829, 43.079834),
+            vec3(284.901093, -608.241760, 43.298950)
+        }
+    },
