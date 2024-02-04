d0be48ec2f636a43a55fa72bd44123f7f55e1906
Domi <106663559+easydomii@users.noreply.github.com>
2024-02-04 17:46:10 +0100

    Create BillingSettings.lua

---
+++ Unternehme/BillingSettings.lua
@@ -0,0 +1,56 @@
+Config.billing = { 
+ 	police = {
+		cancel_bill = 16,
+		company_grade = 16,
+		min_grade = 16,
+		employeecut = 16,
+	},
+	mechanic = {
+		cancel_bill = 6,
+		min_grade = 6,
+		company_grade = 6,
+		employeecut = 6,
+	},
+	ambulance = {
+		cancel_bill = 13,
+		company_grade = 15,
+		min_grade = 13,
+		employeecut = 15,
+	},
+	cali = {
+		cancel_bill = 11,
+		company_grade = 11,
+		min_grade = 11,
+		employeecut = 11,
+	},
+	mg13 = {
+		cancel_bill = 12,
+		company_grade = 12,
+		min_grade = 12,
+		employeecut = 12,
+	},
+	firefighter = {
+		cancel_bill = 6,
+		company_grade = 6,
+		min_grade = 6,
+		employeecut = 6,
+	},
+	goodfellas = {
+		cancel_bill = 4,
+		company_grade = 4,
+		min_grade = 4,
+		employeecut = 4,
+	},
+	greymarkt = {
+		cancel_bill = 5,
+		company_grade = 5,
+		min_grade = 5,
+		employeecut = 5,
+	},
+	goldenpub = {
+		cancel_bill = 1,
+		company_grade = 1,
+		min_grade = 1,
+		employeecut = 1,
+	},
+}
